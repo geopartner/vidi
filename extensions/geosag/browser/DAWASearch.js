c9fff5d02474a69e3f8523f9a7d31cebbfdd5d1a
giovanniborella <rgb@mapster.dk>
2020-11-30 12:27:11 +0100

    implement throttle/debounce

--- extensions/geosag/browser/DAWASearch.js
+++ extensions/geosag/browser/DAWASearch.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import { throttle, debounce } from "throttle-debounce";
 
 function uniqBy(a, key) {
     var seen = {};
@@ -38,6 +39,8 @@ class DAWASearch extends React.Component {
             triggerAtChar: (props.triggerAtChar === undefined ) ? 4 : parseInt(props.triggerAtChar)
 
         };
+        this.autocompleteSearchDebounced = debounce(850, this.autocompleteSearch);
+        this.autocompleteSearchThrottled = throttle(850, this.autocompleteSearch);
 
     }
 
@@ -63,62 +66,82 @@ class DAWASearch extends React.Component {
         var s = _self.state;
         var term = event.target.value;
 
-        //TODO: do smth when there is only 1 result.
-
-        // If not at triggerChar, do nothing
-        if (term.length < s.triggerAtChar) {
-            _self.setState({
-                searchTerm: term,
-                searchResults: []
-            });
-        } else {
-            // run promises here to return stuff from somewhere
-            var calls = [];
-
-            // Anything
-            if (s.enableAdresse) { calls.push(this.callDawa('adresser',term));}
-            if (s.enableMatrikel) { calls.push(this.callDawa('jordstykker',term));}
-
-            // only integers
-            if (!isNaN(parseInt(term))) {
-                if (s.enableESR) { calls.push(this.callDawa('jordstykker',term, 'udvidet_esrejendomsnr'));}
-                if (s.enableBFE) { calls.push(this.callDawa('jordstykker',term, 'bfenummer'));}
-                if (s.enableSFE) { calls.push(this.callDawa('jordstykker',term, 'sfeejendomsnr'));}
+        _self.setState({
+            searchTerm: term,
+            }, () => {
+                const q = s.searchTerm;
+                if (q.length < s.triggerAtChar) {
+                    _self.autocompleteSearchThrottled(s.searchTerm);
+                } else {
+                    _self.autocompleteSearchDebounced(s.searchTerm);
             }
-            
-            // Call the stuff
-            Promise.all(calls)
-                .then( r => {
-                    var results = r;
-                    // Merge all the things
-                    try {
-                        var all = results.flat(1);
-                        var cleaned = [];
+        });
+        
+    };
 
-                        // Dont bring errors
-                        all.forEach(obj => {
-                            if (obj.hasOwnProperty('tekst')) {
-                                cleaned.push(obj);
-                            }
-                        });
+    autocompleteSearch = q => {
+        console.log(`Query: ${q}`)
+        this._fetch(q);
+    };
 
+    _fetch = (q) => {
+        var _self = this;
+        var s = _self.state;
+        var term = s.searchTerm;
+        // run promises here to return stuff from somewhere
+        var calls = [];
+
+        // Anything
+        if (s.enableAdresse) { calls.push(this.callDawa('adresser',term));}
+        if (s.enableMatrikel) { calls.push(this.callDawa('jordstykker',term));}
+
+        // only integers
+        if (!isNaN(parseInt(term))) {
+            if (s.enableESR) { calls.push(this.callDawa('jordstykker',term, 'udvidet_esrejendomsnr'));}
+            if (s.enableBFE) { calls.push(this.callDawa('jordstykker',term, 'bfenummer'));}
+            if (s.enableSFE) { calls.push(this.callDawa('jordstykker',term, 'sfeejendomsnr'));}
+        }
+
+        this.waitingFor = term;
+        console.log(this.waitingFor)
+        
+        // Call the stuff
+        Promise.all(calls)
+            .then( r => {
+                var results = r;
+                
+                // Merge all the things
+                try {
+                    var all = results.flat(1);
+                    var cleaned = [];
+
+                    // Dont bring errors
+                    all.forEach(obj => {
+                        if (obj.hasOwnProperty('tekst')) {
+                            cleaned.push(obj);
+                        }
+                    });
+                    console.log(cleaned);
+                    //Only do something with the term we're expecting
+                    console.log(term)
+                    if (term === this.waitingFor){
                         _self.setState({
                             searchTerm: term,
                             searchResults: uniqBy(cleaned, JSON.stringify).slice(0, s.resultsMax)
                         });
-                    } catch (e) {
-                        _self.setState({
-                            error: e.toString()
-                        });
-                    } 
-                })
-                .catch(err => {
+                    }
+                } catch (e) {
                     _self.setState({
                         error: e.toString()
                     });
+                } 
+            })
+            .catch(err => {
+                _self.setState({
+                    error: e.toString()
                 });
-        }
-    };
+            });
+    }
 
     callDawa = (service, term, specific = undefined) => {
         var s = this.state;
@@ -171,6 +194,12 @@ class DAWASearch extends React.Component {
         return (
             <div>
                 <input type='text' value= { s.searchTerm } onChange={ this.dynamicSearch } placeholder={ s.placeholder } />
+                <button
+                  type="button"
+                  onClick={event => _self.setState({ searchResults: [], searchTerm: '' })}
+                >
+                  Reset
+                </button>
                 <ResultsList
                     results= { s.searchResults }
                     _handleResult={ _self._handleResult }
